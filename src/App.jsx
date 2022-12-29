f16598a74c9102813da142a44ea3a2995bd9ea48
dot-blue <devkl@protonmail.com>
2022-12-29 20:44:10 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import OurMethod from "./components/OurMethod";
 
 function App() {
   return (
     <div className=" h-screen bg-slate-50">
       <Navbar />
       <Hero />
+      <OurMethod />
     </div>
   );
 }
--- src/components/OurMethod.jsx
+++ src/components/OurMethod.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
+import perk1 from "../assets/perk1.svg";
+import perk2 from "../assets/perk2.svg";
+import perk3 from "../assets/perk3.svg";
+
 function OurMethod() {
-  return <div></div>;
+  return (
+    <div
+      className=" bg-purple-200  w-full 
+    "
+    >
+      <div className="max-w-[1240px] m-auto ">
+        <h2 className="text-4xl font-bold py-6 px-4">
+          FitFusion method really works.{" "}
+        </h2>
+
+        <div className="flex flex-col md:flex-row items-center justify-between gap-4 ">
+          <div className=" flex flex-col   w-full items-center p-6 ">
+            <p className="">Tailored to each individual</p>
+            <p className="">
+              Our team of experts will work with you to create a personalized
+              workout plan and nutrition plan based on your specific goals and
+              needs
+            </p>
+            <img src={perk1} alt="perk1" />
+          </div>
+
+          <div className=" flex flex-col  w-full items-center p-6">
+            <p>Supportive community</p>
+            <p>
+              Losing weight can be a tough journey, but you don't have to go it
+              alone. Our community of clients and trainers offer support and
+              motivation to help you stay on track and reach your goals.{" "}
+            </p>
+            <img src={perk2} alt="perk2" />
+          </div>
+
+          <div className=" flex flex-col   w-full items-center p-6">
+            <p>Challenging workouts</p>
+            <p>
+              Our workouts are challenging, but achievable: We believe in
+              pushing our clients to their limits, but not to the point of
+              burnout.
+            </p>
+            <img src={perk3} alt="perk3" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default OurMethod;
