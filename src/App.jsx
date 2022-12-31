bfcaf2a91a0b2f174598475b3014324ba451fb49
dot-blue <devkl@protonmail.com>
2022-12-31 14:09:13 +0200

    c

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import OurMethod from "./components/OurMethod";
 import MobileApp from "./components/MobileApp";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <OurMethod />
       <MobileApp />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import logo from "../assets/FitFusion.svg";
+import fb from "../assets/fb-icon.svg";
+import twitter from "../assets/twitter-icon.svg";
+import ig from "../assets/instagram-icon.svg";
+import tiktok from "../assets/tiktok-icon.svg";
+
+function Footer() {
+  return (
+    <div className=" bg-purple-400 flex flex-col justify-center px-4">
+      <div className=" max-w-[1240px] flex flex-col md:flex-row  items-center justify-between  ">
+        <div className="md:basis-1/3 w-3/4">
+          <p className="text-xl font-bold py-2 md:text-3xl">
+            Stay ahead of the game{" "}
+          </p>
+          <p className="">
+            Subscribe to our weekly newsletter and get access to special
+            discounts and promotions
+          </p>
+        </div>
+
+        <div className="bg-gray-200/40 p-6 my-4 flex flex-col ">
+          <div className="flex gap-4 md:basis-2/3 ">
+            {" "}
+            <input
+              type="text"
+              placeholder="Email address"
+              className="h-10 pl-4 "
+            />
+            <button className="bg-purple-500 px-4 text-white">Subscribe</button>
+          </div>
+
+          <div className="flex pt-2 gap-2">
+            <input type="checkbox" className="" />
+            <p className="text-xs">
+              I agree to receive marketing email from FitFusion.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between py-4  items-center  ">
+        <img src={logo} alt="" className="h-6" />
+
+        <div className="flex gap-4 items-center">
+          <img src={fb} alt="" />
+          <img src={ig} alt="" />
+          <img src={twitter} alt="" />
+          <img src={tiktok} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
