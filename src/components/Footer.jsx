af7c1fe229b996f904e60c04242c0a167771fb82
dot-blue <devkl@protonmail.com>
2022-12-31 15:54:37 +0200

    c

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,47 +7,52 @@ import tiktok from "../assets/tiktok-icon.svg";
 
 function Footer() {
   return (
-    <div className=" bg-slate-100 flex flex-col justify-center px-4 py-6">
-      <div className=" max-w-[1240px] flex flex-col md:flex-row  items-center justify-between  ">
-        <div className="md:basis-1/3 px-4 ">
-          <p className="text-xl font-bold py-2 md:text-3xl ">
-            Stay ahead of the game{" "}
-          </p>
-          <p className="">
-            Subscribe to our weekly newsletter and get access to special
-            discounts and promotions
-          </p>
-        </div>
-
-        <div className=" p-6 my-4 flex flex-col ">
-          <div className="flex  gap-2 ">
-            {" "}
-            <input
-              type="text"
-              placeholder="Email address"
-              className="h-10 pl-4 rounded-md"
-            />
-            <button className="bg-purple-500 px-4 text-white rounded-md">
-              Subscribe
-            </button>
-          </div>
-
-          <div className="flex pt-2 gap-2">
-            <input type="checkbox" className="" />
-            <p className="text-xs">
-              I agree to receive marketing email from FitFusion.
+    <div className=" bg-slate-100 flex flex-col px-4 py-6 items-center ">
+      <div className=" max-w-[1240px]">
+        <div className=" max-w-[1240px] flex flex-col md:flex-row  items-center    ">
+          <div className="md:basis-2/4 px-4 ">
+            <p className="text-xl font-bold py-2 md:text-3xl ">
+              Stay ahead of the game{" "}
+            </p>
+            <p className="">
+              Subscribe to our weekly newsletter and get access to special
+              discounts and promotions
             </p>
           </div>
-        </div>
-      </div>
-      <div className="flex justify-between   items-center px-4">
-        <img src={logo} alt="" className="h-6" />
 
-        <div className="flex gap-4 items-center">
-          <img src={fb} alt="" />
-          <img src={ig} alt="" />
-          <img src={twitter} alt="" />
-          <img src={tiktok} alt="" />
+          <div className=" p-6 my-4 flex flex-col  bg-purple-100/80 rounded-md md:basis-2/4">
+            <div className="flex justify-between gap-2 ">
+              {" "}
+              <input
+                type="text"
+                placeholder="Email address"
+                className="h-10 pl-4 rounded-md w-full"
+              />
+              <button className="bg-purple-500 px-4 text-white rounded-md">
+                Subscribe
+              </button>
+            </div>
+
+            <div className="flex pt-2 gap-2">
+              <input type="checkbox" className="" />
+              <p className="text-xs">
+                I agree to receive marketing email from FitFusion.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex  px-4 justify-between">
+          <div>
+            {" "}
+            <img src={logo} alt="" className="h-6" />
+          </div>
+
+          <div className="flex gap-4 items-center">
+            <img src={fb} alt="" />
+            <img src={ig} alt="" />
+            <img src={twitter} alt="" />
+            <img src={tiktok} alt="" />
+          </div>
         </div>
       </div>
     </div>
